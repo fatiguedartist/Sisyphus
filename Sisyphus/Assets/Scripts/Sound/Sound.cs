4a676175a94e010177122cee65a6730ed71669ed
Justin Nearing <justinfnearing@gmail.com>
2016-01-31 00:50:31 -0800

    Randomized music plays, no fancy reset

---
+++ Sisyphus/Assets/Scripts/Sound/Sound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Sound : MonoBehaviour {
+
+    AudioSource[] childAudioSources;
+    public AudioSource CurrentSong;
+
+	// Use this for initialization
+	void Start () {
+        childAudioSources = GetComponentsInChildren<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CurrentSong == null || !CurrentSong.isPlaying)
+        {
+            PlayMusic();
+        }
+	}
+
+    void PlayMusic()
+    {
+        CurrentSong = childAudioSources.SelectRandom();
+        CurrentSong.Play();
+    }
+}
