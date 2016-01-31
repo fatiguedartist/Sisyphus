814c3f654bc5267d61695c89bd857ea8bec56201
Francis Griffiths-Keam <fatiguedartist@gmail.com>
2016-01-31 11:28:23 -0800

    The runestone now generates random sound.

---
+++ Sisyphus/Assets/PlayRandomAudio.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayRandomAudio : MonoBehaviour {
+
+    public List<AudioClip> audioClips;
+    public AudioSource audioSource;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!audioSource.isPlaying)
+	    {
+	        audioSource.PlayOneShot(audioClips.SelectRandom());
+	    }
+	}
+}
