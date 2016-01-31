a4ca181be44377d976c0250496d72c2e3f4f1b26
Francis Griffiths-Keam <fatiguedartist@gmail.com>
2016-01-31 11:10:51 -0800

    Added custom footsteps and missing UV scroll.

---
+++ Sisyphus/Assets/UVScroll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UVScroll : MonoBehaviour
+{
+    private Material mat;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    mat = GetComponent<MeshRenderer>().sharedMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    mat.SetTextureOffset("_DetailAlbedoMap", Vector2.right * Time.time * 0.1f);
+	}
+}
